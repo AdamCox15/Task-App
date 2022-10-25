f3d142738f2efb58ec04a9db897af5887bd75e0a
Adam Cox <adamcox.dev@gmail.com>
2022-10-25 11:47:46 -0600

    Second commit building database

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import null
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.model):
+    id = db.Column(db.Integer,primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
 
 if __name__=="__main__":
     app.run(debug=True)
